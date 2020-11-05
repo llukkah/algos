49d16083c4a2a2151f2d805f69033d2a6720116d
llukkah <llukkah.rey@gmail.com>
2020-11-05 08:08:22 -0500

    added repeated string and electronics-shop to HackerRank

---
+++ repeated-string.js
@@ -0,0 +1,14 @@
+/////////////////////////////////////////////////
+////////Repeat a given string (`str`)///////////
+//////// a given number of times (`num`)./////////
+////// The output should return a string.////////
+//////////////////////////////////////////////////
+
+const repeatString = (str, num) => {
+    let repeatedString = "";
+    while (num > 0) {
+      repeatedString += str;
+      num--;
+    }
+    return repeatedString;
+  }
