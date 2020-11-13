0cfa6c5a28931f310684fccbd65da6f07a33ea21
llukkah <llukkah.rey@gmail.com>
2020-11-13 09:18:01 -0500

    added birthday.js to hackerrank and largest of each to algos

---
+++ HackerRank/birthday.js
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+function birthday(s, d, m) {
+ let count = 0
+
+for(let i = 0; i < s.length; i++) {
+
+    let arr = s.slice(0 + i, m + i)
+
+    if(arr.reduce((a,b) => a+ b) === d) {           
+        count++         
+    }      
+
+}
+return count
+}
+
+
