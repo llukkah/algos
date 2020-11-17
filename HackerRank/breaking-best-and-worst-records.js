05f440a0389bc47a3345f556674efa047f32d1d7
llukkah <llukkah.rey@gmail.com>
2020-11-17 15:18:10 -0500

    added breaking worst and best records to hackerRank

---
+++ HackerRank/breaking-best-and-worst-records.js
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+function breakingRecords(scores) {
+  let highestScore = scores[0];
+  let lowestScore = scores[0];
+  let highestScoreChanged = 0;
+  let lowestScoreChanged = 0;
+  let highestLowest = [];
+  for (let i=1; i < scores.length; i+=1){
+    if (scores[i] > highestScore){
+      highestScore = scores[i];
+      highestScoreChanged +=1; 
+    } else if (scores[i] < lowestScore){
+      lowestScore = scores[i];
+      lowestScoreChanged +=1; 
+    }
+  }
+      highestLowest.push(highestScoreChanged);
+      highestLowest.push(lowestScoreChanged);
+      return highestLowest
+}
+
+
