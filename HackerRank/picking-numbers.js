b50b3a5b4f54cc2810c40ba845e202f3512a16c2
llukkah <llukkah.rey@gmail.com>
2020-11-18 15:13:52 -0500

    added picking numbers to hackerRank

---
+++ HackerRank/picking-numbers.js
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/picking-numbers/problem
+pickingNumbers = (arr) => {
+  let sorted = a.sort((a, b) => a - b);
+  let counter = 0;
+  let max = 0;
+  for (let i = 0; i < sorted.length; i++){
+    if (sorted[i+1] - sorted[i]  <= 1) {
+      counter += 1;
+    }
+    if (counter > max) {
+      max = counter
+    }
+  }
+  return max;
+}
+
+console.log(pickingNumbers([1, 2, 2, 3, 1, 2]))
