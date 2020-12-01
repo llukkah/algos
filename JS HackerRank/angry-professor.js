b739b3e12bcc5224aaa44c0b0323682a35f3456b
llukkah <llukkah.rey@gmail.com>
2020-12-01 10:24:41 -0500

    added angry professor to HackerRank

---
+++ JS HackerRank/angry-professor.js
@@ -0,0 +1,25 @@
+// https://www.hackerrank.com/challenges/angry-professor/problem
+
+// A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, the professor decides to cancel class if fewer than some number of students are present when class starts. Arrival times go from on time (arrivalTime <= 0) to arrived late (arrivalTime >= 0).
+
+// Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled.
+
+// Function Description
+// Complete the angryProfessor function in the editor below. It must return YES if class is cancelled, or NO otherwise.
+
+// angryProfessor has the following parameter(s):
+// int k: the threshold number of students
+// int a[n]: the arrival times of the n students
+
+// Returns
+// string: either YES or NO
+
+
+
+function angryProfessor(k, a) {
+
+
+}
+
+console.log(angryProfessor(3, [-1, -3, 4, 2])) // YES
+console.log(angryProfessor(2, [0, -1, 2, 1])) // NO
