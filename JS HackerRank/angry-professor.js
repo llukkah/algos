46d4d0edab849d8f92d4e1014658172f0ad2f837
llukkah <llukkah.rey@gmail.com>
2020-12-01 10:53:42 -0500

    added angry professor to HackerRank

--- JS HackerRank/angry-professor.js
+++ JS HackerRank/angry-professor.js
@@ -14,12 +14,34 @@
 // Returns
 // string: either YES or NO
 
-
+//PSEUDOCODE
+//sort the array
+//create counter
+//iterate through array to find on time arrival times (<= 0)
+//if arrival time[i] <=0, counter +=1
+//if counter is >= k, return No.
 
 function angryProfessor(k, a) {
-
-
+    let counter = 0;
+    let sortedArr = a.sort(function(a,b) {return a-b})
+    // console.log(sortedArr)
+    for (let i = 0; i <= sortedArr.length; i += 1) {
+        // console.log(sortedArr[i])
+        if (sortedArr[i] < 0 || sortedArr[i] === 0) {
+            counter += 1;
+            // console.log('sortedArr i = ' + sortedArr[i])
+            // console.log('counter = ' + counter)
+        // } if(counter >= k) {
+        //     return 'NO'
+        // } else {
+        //     return 'YES'
+        }
+                } if(counter >= k) {
+            return 'NO'
+        } else {
+            return 'YES'
+    }
 }
 
 console.log(angryProfessor(3, [-1, -3, 4, 2])) // YES
-console.log(angryProfessor(2, [0, -1, 2, 1])) // NO
+// console.log(angryProfessor(2, [0, -1, 2, 1])) // NO
