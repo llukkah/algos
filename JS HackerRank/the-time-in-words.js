565a630a05632d6622b4b5d66fb3635ae6fd5ffc
llukkah <llukkah.rey@gmail.com>
2020-12-03 10:20:57 -0500

    added time in words to HackerRank

---
+++ JS HackerRank/the-time-in-words.js
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/the-time-in-words/problem
+
+//PESUDOCODE
+//make words array to push each number / word in
+//if statement  to determine oclock, quarter, past, actual number
+//push into words array
+//convert array to string into a variable
+//return string
+
+function timeInWords(h, m) {
+
+
+}
+
+console.log(timeInWords(5,47)) //thirteen minutes to six
+// console.log(timeInWords(3,00)) //three o' clock
+// console.log(timeInWords(7,15)) //quarter past seven
