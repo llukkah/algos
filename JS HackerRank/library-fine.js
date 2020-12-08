915da55c4a1b0cfd1b24d4dcda0d659c80fe1976
llukkah <llukkah.rey@gmail.com>
2020-12-08 14:14:19 -0500

    completed library fine

--- JS HackerRank/library-fine.js
+++ JS HackerRank/library-fine.js
@@ -38,22 +38,6 @@
 // Per the library's fee structure, we know that our fine will be 15 Hackos x (# of days late). We then print the result of 15 x (d1-d2) = 15 x (9 - 6) = 45 as our output.
 
 function libraryFine(d1, m1, y1, d2, m2, y2) {
-//   if (y2 !== y1) {
-//     return "10000";
-//   }
-//   if (y2 == y1 && m2 !== m1) {
-//     return 500 * (m1 - m2);
-//   }
-//   if (y2 == y1 && m2 == m1 && d2 !== d1) {
-//     return 15 * (d1 - d2);
-//   }
-//   if (y2 == y1 && m2 == m1 && d2 == d1) {
-//     return 0;
-//   }
-//   if (y2 == y1 && m2 == m1 && d2 > d1) {
-//     return 0;
-//   }
-
   if (y1 < y2) return 0;
   if (y1 > y2) return 10000;
 
