34f7bff583c5c5f4a5b700a28fa3f04b9582049d
llukkah <llukkah.rey@gmail.com>
2020-12-29 11:00:17 -0500

    edited diagonal difference in hackerrank

--- JS HackerRank/diagonal-difference.js
+++ JS HackerRank/diagonal-difference.js
@@ -1,4 +1,44 @@
 // https://www.hackerrank.com/challenges/diagonal-difference/problem
+
+// FUNCTION DESCRIPTION
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+// For example, the square matrix  is shown below:
+// 1 2 3
+// 4 5 6
+// 9 8 9
+// The left-to-right diagonal = 1 + 5 + 9 = 15. The right to left diagonal = 3 + 5 + 9 = 17. Their absolute difference is |15 - 17| = 2.
+
+// INPUT FORMAT
+// The first line contains a single integer, n, the number of rows and columns in the square matrix arr.
+// Each of the next n lines describes a row, arr[i], and consists of n space-separated integers arr[i][j].
+
+// OUTPUT FORMAT
+// Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+// SAMPLE INPUT
+// 3
+// 11 2 4
+// 4 5 6
+// 10 8 -12
+
+// SAMPLE OUTPUT
+// 15
+
+// EXPLANATION
+// The primary diagonal is:
+// 11
+//    5
+//      -12
+// Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+// The secondary diagonal is:
+//      4
+//    5
+// 10
+// Sum across the secondary diagonal: 4 + 5 + 10 = 19
+// Difference: |4 - 19| = 15
+
 function diagonalDifference(arr) {
   let firstDiagonal = 0;
   let secondDiagonal = 0;
