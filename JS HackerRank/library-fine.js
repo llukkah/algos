79ef06e272791fc60c1e13fb347d8db93d92b3ee
llukkah <llukkah.rey@gmail.com>
2020-12-30 10:14:18 -0500

    edited function description

--- JS HackerRank/library-fine.js
+++ JS HackerRank/library-fine.js
@@ -1,5 +1,6 @@
 //https://www.hackerrank.com/challenges/library-fine/problem
 
+// FUNCTION DESCRIPTION
 //Your local library needs your help! Given the expected and actual return dates for a library book, create a program that calculates the fine (if any). The fee structure is as follows:
 
 //1. If the book is returned on or before the expected return date, no fine will be charged (i.e.: .fine = 0)
@@ -9,21 +10,18 @@
 
 //Charges are based only on the least precise measure of lateness. For example, whether a book is due January 1, 2017 or December 31, 2017, if it is returned January 1, 2018, that is a year late and the fine would be 10,000 Hackos.
 
-// FUNCTION DESCRIPTION
-// Complete the libraryFine function.
-
-// libraryFine has the following parameter(s):
+// INPUT FORMAT
 // d1, m1, y1: returned date day, month and year, each an integer
 // d2, m2, y2: due date day, month and year, each an integer
 
-// RETURNS
+// OUTPUT FORMAT
 // int: the amount of the fine or 0 if there is none.
 
-//SAMPLE INPUT
+// SAMPLE INPUT
 // 9 6 2015
 // 6 6 2015
 
-//SAMPLE OUTPUT
+// SAMPLE OUTPUT
 // 45
 
 //EXPLANATION
