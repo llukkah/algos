71d91d56c02ae7b88538cd022b01ecbbec1e6e13
llukkah <llukkah.rey@gmail.com>
2020-12-30 10:08:33 -0500

    edited function description

--- JS HackerRank/compare-the-triplets.js
+++ JS HackerRank/compare-the-triplets.js
@@ -1,5 +1,6 @@
 //https://www.hackerrank.com/challenges/compare-the-triplets/problem
 
+// FUNCTION DESCRIPTION
 // Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
 
 // The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
@@ -14,7 +15,6 @@
 // Given a and b, determine their respective comparison points.
 
 // EXAMPLE
-
 // a = [1, 2, 3]
 // b = [3, 2, 1]
 // For elements *0*, Bob is awarded a point because a[0] .
@@ -22,19 +22,13 @@
 // Finally, for elements 2, a[2] > b[2] so Alice receives a point.
 // The return array is [1, 1] with Alice's score first and Bob's second.
 
-// FUNCTION DESCRIPTION
-// Complete the function compareTriplets in the editor below.
-
-// compareTriplets has the following parameter(s):
+// INPUT FORMAT
 // int a[3]: Alice's challenge rating
 // int b[3]: Bob's challenge rating
 
-// Return
+// OUTPUT FORMAT
 // int[2]: Alice's score is in the first position, and Bob's score is in the second.
 
-// INPUT FORMAT
-// The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
-// The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
 
 //SAMPLE INPUT
 // 5 6 7
