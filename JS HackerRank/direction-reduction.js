71439e036c7c247a7d05648f73845b4b777e27a1
llukkah <llukkah.rey@gmail.com>
2020-12-30 10:10:52 -0500

    edited function description

--- JS HackerRank/direction-reduction.js
+++ JS HackerRank/direction-reduction.js
@@ -2,7 +2,7 @@
 
 // 'NORTH' and 'SOUTH' are opposite directions, as are 'EAST' and 'WEST'.  Going in one directiosn and coming back in the opposite direction leads to going nowhere.  Someone else also has these directiosn to the treasure and you need to get there first.  Since the directions are long, you decide to write a program to figure out the fastest and most direct route to the treasure.
 
-// Write a function that will take a list of strings and will return a list of strings wit hthe unneeded directions removed. (NORTH<->SOUTH or EAST<->WEST side by side).
+// Write a function that will take a list of strings and will return a list of strings with the unneeded directions removed. (NORTH<->SOUTH or EAST<->WEST side by side).
 
 // SAMPLE INPUT
 // ['NORTH', 'EAST', 'WEST', 'SOUTH', 'WEST', 'SOUTH', 'NORTH', 'WEST']
