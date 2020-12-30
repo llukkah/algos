98dcd4feb4c2e7f13adcf6b82cffb58bb7e6f7ab
llukkah <llukkah.rey@gmail.com>
2020-12-30 09:29:40 -0500

    added plus minus description

--- JS HackerRank/plus-minus.js
+++ JS HackerRank/plus-minus.js
@@ -1,5 +1,6 @@
 // https://www.hackerrank.com/challenges/plus-minus/problem
 
+// FUNCTION DESCRIPTION
 // Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
 
 // Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10^-4 are acceptable.
@@ -11,23 +12,17 @@
 // 0.400000
 // 0.200000
 
-// FUNCTION DESCRIPTION
-
-// plusMinus has the following parameter(s):
+// INPUT FORMAT
 // int arr[n]: an array of integers
 
 // Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with 6 digits after the decimal. The function should not return a value.
-
-// INPUT FORMAT
-// The first line contains an integer, n, the size of the array.
-// The second line contains n space-separated integers that describe arr[n].
 
 // Constraints
 // 0 < n <= 100
 // -100 <= arr[i] <= 100
 
 // OUTPUT FORMAT
-// Print the following  lines, each to  decimals:
+// Print the following  3lines, each to 6 decimals:
 // proportion of positive values
 // proportion of negative values
 // proportion of zeros
