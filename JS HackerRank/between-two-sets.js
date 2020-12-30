3218d15e9053b5b8194b6bd4e04820167cdce019
llukkah <llukkah.rey@gmail.com>
2020-12-30 10:02:55 -0500

    edited between tow sets function desxcription

--- JS HackerRank/between-two-sets.js
+++ JS HackerRank/between-two-sets.js
@@ -1,5 +1,6 @@
 //https://www.hackerrank.com/challenges/between-two-sets/problem
 
+// FUNCTION DESCRIPTION
 // There will be two arrays of integers. Determine all integers that satisfy the following two conditions:
 
 // The elements of the first array are all factors of the integer being considered
@@ -15,30 +16,22 @@
 // For the second value: 12%2 = 0  12 %6 = 0   24%12 = 0   36%12 = 0
 // Return 2.
 
-// Function Description
-// Complete the getTotalX function in the editor below. It should return the number of integers that are betwen the sets.
-
-// getTotalX has the following parameter(s):
+// INPUT FORMAT
 // int a[n]: an array of integers
 // int b[m]: an array of integers
 
-// Returns
+// OUTPUT FORMAT
 // The number of integers that are between the sets
 
-// Input Format
-// The first line contains two space-separated integers, n and m, the number of elements in arrays a and b.
-// The second line contains n distinct space-separated integers a[i] where 0 <= i < n.
-// The third line contains m distinct space-separated integers b[j] where 0<= j < m.
-
-// Sample input
+// SAMPLE INPUT
 // 2 3
 // 2 4
 // 16 32 96
 
-// Sample Output
+// SAMPLE OUTPUT
 // 3
 
-// Explanation
+// EXPLANATION
 // 2 and 4 divide evenly into 4, 8, 12 and 16.
 // 4, 8 and 16 divide evenly into 16, 32, 96.
 // 4, 8 and 16 are the only three numbers for which each element of a is a factor and each is a factor of all elements of b.
