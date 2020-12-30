56ecf1825355eb04fb2131bfb38e20f4fbb4d1c9
llukkah <llukkah.rey@gmail.com>
2020-12-30 10:12:51 -0500

    edited function description

--- JS HackerRank/find-digits.js
+++ JS HackerRank/find-digits.js
@@ -1,5 +1,6 @@
 // https://www.hackerrank.com/challenges/find-digits/problem
 
+// FUNCTION DESCRIPTION
 // An integer d is a divisor of an integer n if the remainder of n / d = 0.
 
 // Given an integer, for each digit that makes up the integer determine whether it is a divisor. Count the number of divisors occurring within the integer.
@@ -11,17 +12,20 @@
 //n = 10
 // Check whether 1 and 0 are divisors of 10. 1 is, but 0 is not. Return 1.
 
-// FUNCTION DESCRIPTION
-// Complete the findDigits function in the editor below.
-
-// findDigits has the following parameter(s):
+// INPUT FORMAT
 // int n: the value to analyze
 
-// RETURNS
+// OUTPUT FORMAT
 // int: the number of digits in n that are divisors of n.
 
-// INPUT FORMAT
-// n = integer to examine.
+// SAMPLE INPUT
+// 2
+// 12
+// 1012
+
+// SAMPLE OUTPUT
+// 2
+// 3
 
 function findDigits(n) {
   let digitArray = n.toString();
