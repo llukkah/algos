d780b6f5f5409ce3a47842059060658e6c5361e5
llukkah <llukkah.rey@gmail.com>
2020-12-30 10:03:53 -0500

    edited birthday chocolate function description

--- JS HackerRank/birthday.js
+++ JS HackerRank/birthday.js
@@ -1,5 +1,6 @@
 // https://www.hackerrank.com/challenges/the-birthday-bar/problem
 
+// FUNCTION DESCRIPTION
 // Given a chocolate bar, two children, Lily and Ron, are determining how to share it. Each of the squares has an integer on it.
 
 // Lily decides to share a contiguous segment of the bar selected such that:
@@ -10,18 +11,10 @@
 
 // Consider the chocolate bar as an array of squares, s = [2, 2, 1, 3, 2]. She wants to find segments summing to Ron's birth day, d = 4 with a length equalling his birth month, m = 2. In this case, there are two segments meeting her criteria: [2, 2] and [1,3].
 
-// FUNCTION DESCRIPTION
-
-// birthday has the following parameter(s):
+// INPUT FORMAT
 // s: an array of integers, the numbers on each of the squares of chocolate
 // d: an integer, Ron's birth day
 // m: an integer, Ron's birth month
-
-// INPUT FORMAT
-
-// The first line contains an integer n, the number of squares in the chocolate bar.
-// The second line contains n space-separated integers s[i], the numbers on the chocolate squares where .
-// The third line contains two space-separated integers, d and m, Ron's birth day and his birth month.
 
 // OUTPUT FORMAT
 // Print an integer denoting the total number of ways that Lily can portion her chocolate bar to share with Ron.
