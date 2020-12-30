6cc17390405354190c379bac748eb56e77e5e62b
llukkah <llukkah.rey@gmail.com>
2020-12-30 10:11:37 -0500

    edited function description

--- JS HackerRank/electronics-shop.js
+++ JS HackerRank/electronics-shop.js
@@ -1,5 +1,6 @@
 // https://www.hackerrank.com/challenges/electronics-shop/problem
 
+// FUNCTION DESCRIPTION
 // A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a give budget. Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to buy both items, return -1.
 
 // Example
@@ -9,20 +10,13 @@
 
 // The person can buy a 40 keyboard + 12 USB drive = 52, or a 50 keyboard + 8 USB Drive = 58. Choose the latter as the more expensive option and return 58.
 
-// FUNCTION DESCRIPTION
-
-// getMoneySpent has the following parameter(s):
+// INPUT FORMAT
 // int keyboards[n]: the keyboard prices
 // int drives[m]: the drive prices
 // int b: the budget
 
-// Returns
+// OUTPUT FORMAT
 // int: the maximum that can be spent, or  if it is not possible to buy both items
-
-// INPUT FORMAT
-// The first line contains three space-separated integers b, n, and m, the budget, the number of keyboard models and the number of USB drive models.
-// The second line contains n space-separated integers keyboard[i], the prices of each keyboard model.
-// The third line contains m space-separated integers drives, the prices of the USB drives.
 
 // SAMPLE INPUT 0
 // 10 2 3
