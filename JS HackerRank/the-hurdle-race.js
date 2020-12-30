a911a2d5584157a3a3ed9334d6a054a07cedf1cd
llukkah <llukkah.rey@gmail.com>
2020-12-30 09:42:57 -0500

    added hurdle race descrioption

--- JS HackerRank/the-hurdle-race.js
+++ JS HackerRank/the-hurdle-race.js
@@ -1,5 +1,6 @@
 //https://www.hackerrank.com/challenges/the-hurdle-race/problem
 
+// FUNCTION DESCRIPTION
 // A video player plays a game in which the character competes in a hurdle race. Hurdles are of varying heights, and the characters have a maximum height they can jump. There is a magic potion they can take that will increase their maximum jump height by 1 unit for each dose. How many doses of the potion must the character take to be able to jump all of the hurdles. If the character can already clear all of the hurdles, return 0.
 
 // Example
@@ -8,33 +9,26 @@
 
 // The character can jump 1 unit high initially and must take 3 - 1 = 2 doses of potion to be able to jump all of the hurdles.
 
-// Function Description
-// Complete the hurdleRace function in the editor below.
-
-// hurdleRace has the following parameter(s):
+// INPUT FORMAT
 // int k: the height the character can jump naturally
 // int height[n]: the heights of each hurdle
 
-// Returns
+// OUTPUT FORMAT
 // int: the minimum number of doses required, always 0 or more
-
-// Input Format
-// k = the maximum height the character can jump naturally.
-// The second line contains  integers height[i]  where 0 <= i < n.
 
 // Constraints
 // 1 <= n, k <= 100
 // 1 <= height[i] <= 100
 
-// Sample Input
-// 4
+// Sample Input 0
 // 1 6 3 5 2
+
 // Sample Output 0
 // 2
 
 // Sample Input 1
-// 7
 // 2 5 4 5 2
+
 // Sample Output 1
 // 0
 
