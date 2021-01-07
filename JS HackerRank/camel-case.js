e00f2e1321d99af650451e74c12c5312dd09c54f
llukkah <llukkah.rey@gmail.com>
2021-01-07 07:28:25 -0500

    added camel case to hacker rank

---
+++ JS HackerRank/camel-case.js
@@ -0,0 +1,39 @@
+// https://www.hackerrank.com/challenges/camelcase/problem
+
+// FUNCTION DESCRIPTION
+// There is a sequence of words in CamelCase as a string of letters, s, having the following properties:
+
+// It is a concatenation of one or more words consisting of English letters.
+// All letters in the first word are lowercase.
+// For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+// Given s, determine the number of words in s.
+
+// EXAMPLE
+// s = oneTwoThree
+// There are 3 words in the string: 'one', 'Two', 'Three'.
+
+// INPUT FORMAT
+// string s: the string to analyze
+
+// OUTPUT FORMAT
+// int: the number of words in s
+
+// SAMPLE INPUT
+// saveChangesInTheEditor
+
+// SAMPLE OUTPUT
+// 5
+
+// EXPLANATION
+// String s contains five words:
+
+// save
+// Changes
+// In
+// The
+// Editor
+
+function camelcase(s) {
+
+
+}
